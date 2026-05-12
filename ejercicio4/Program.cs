aeedde57febc2ce0dabef2dc1a6a43f51955c0ed
gabrielito <gomcarrillo@uamv.edu.ni>
2026-05-12 17:54:04 -0600

    semana 9

---
+++ ejercicio4/Program.cs
@@ -0,0 +1,30 @@
+﻿// ventas por categoría, total de libros, categoría con más ventas y promedio
+int[] ventas = new int[5];
+int total = 0;
+double promedio = 0;
+for (int i = 0; i < ventas.Length; i++)
+{
+    Console.WriteLine($"Ingrese la cantidad de libros vendidos en la categoría {i + 1}:");
+    ventas[i] = int.Parse(Console.ReadLine()!);
+}
+for (int i = 0; i < ventas.Length; i++)
+{
+    total += ventas[i];
+}
+
+promedio = (double)total / ventas.Length;
+int categoriaMasVendida = ventas[0];
+int indiceCategoria = 1;
+for (int i = 0; i < ventas.Length; i++)
+{
+    if (ventas[i] > categoriaMasVendida)
+    {
+        categoriaMasVendida = ventas[i];
+        indiceCategoria = i + 1; 
+    }
+}
+
+Console.WriteLine("RESULTADOS");
+Console.WriteLine($"Total de libros vendidos: {total}");
+Console.WriteLine($"Promedio de ventas por categoría: {promedio:F2}");
+Console.WriteLine($"La categoría con más ventas es la número {indiceCategoria} (con {categoriaMasVendida} libros vendidos gracias uwu)");
---
+++ ejercicio5/Program.cs
@@ -0,0 +1,26 @@
+﻿// notas de 8 estudiantes, aprobados, reprobados y promedio
+double[] notas = new double[8];
+double total = 0;
+int contadorReprobados = 0;
+for (int i = 0; i < notas.Length; i++)
+{
+    Console.WriteLine($"Ingrese la nota del estudiante {i + 1}:");
+    notas[i] = double.Parse(Console.ReadLine()!);
+    total += notas[i];
+}
+Console.WriteLine("NOTAS APROBADAS (60 o más)");
+for (int i = 0; i < notas.Length; i++)
+{
+    if (notas[i] >= 60)
+    {
+        Console.WriteLine($"Estudiante {i + 1}: {notas[i]} has aprobado sos tuani");
+    }
+    else
+    {
+       contadorReprobados++;
+    }
+}
+double promedioGeneral = total / notas.Length;
+Console.WriteLine("Datos del grupo");
+Console.WriteLine($"Promedio general del grupo: {promedioGeneral:F2}");
+Console.WriteLine($"Cantidad de estudiantes reprobados: {contadorReprobados}");
